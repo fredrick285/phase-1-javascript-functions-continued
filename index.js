9854dd5d4924cd0cd90d3a13127acc537b1a7176
Fredrick <fredrick.lokidor@student.moringaschool.com>
2022-06-11 22:12:44 +0300

    commit tree 87c9faaf5b1c620d52da5e3dcd65242b205f8595

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(myStrings="roller-skate"){
+    return `This Saturday, I want to ${myStrings}!`
+}
+
+function mondayWork(myStrings="go to the office"){
+    return `This Monday, I will ${myStrings}.`
+}
+
+function wrapAdjective(myStings="*") {
+    return function (myAdj = "special") {
+        return `You are ${myStrings}${myAdj}${myStrings}!`;
+      };
+}
+  
+const encouragingPromptFunction = wrapAdjective()("a hard worker"); 
